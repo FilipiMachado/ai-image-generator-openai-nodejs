2afd35996b012329098704df6e2abd3e72398483
fil <filipisee@hotmail.com>
2022-12-23 10:22:57 -0300

    finished backend

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -9,13 +9,14 @@ const openai = new OpenAIApi(configuration);
 const generateImage = async (req, res) => {
   const { prompt, size } = req.body;
 
-  const imageSize = size === "small"
+  const imageSize =
+    size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
 
   try {
     const response = await openai.createImage({
       prompt: prompt,
       n: 1,
-      size: size,
+      size: imageSize,
     });
 
     const imageUrl = response.data.data[0].url;
