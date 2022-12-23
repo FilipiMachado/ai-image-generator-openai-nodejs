287eafb6f50634852515e51104a419b77a4f2a00
fil <filipisee@hotmail.com>
2022-12-23 11:32:35 -0300

    working on generate image request

---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+document.querySelector("#image-form").addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const prompt = document.querySelector("#prompt").value;
+  const size = document.querySelector("#size").value;
+
+  if (prompt === "") {
+    alert("Please add some text.");
+    return;
+  }
+
+  generateImageRequest(prompt, size);
+}
+
+async function generateImageRequest(prompt, size) {
+  try {
+    showSpinner();
+
+    const response = await fetch("/openai/generateimage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "apllication/json",
+      },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
+
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("That image could not be generated");
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    removeSpinner();
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
+}
+
+function showSpinner() {
+  document.querySelector("#spinner").classList.add("show");
+}
+
+function removeSpinner() {
+  document.querySelector("#spinner").classList.remove("show");
+}
